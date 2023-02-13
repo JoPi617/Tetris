00471220b94c239230d6fb7f7bf1e8173f9ed114
jhp33 <jhp33c@gmail.com>
2023-02-13 20:42:01 +0000

    Add project files.

---
+++ WpfApp1/Game.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Tetris;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for Game.xaml
+/// </summary>
+public partial class Game : Window
+{
+    private Piece _fallingPiece;
+    private bool[,] _board;
+    private int _height;
+    private int _width;
+    private DispatcherTimer _gravityTimer;
+    private List<string> _pieces = new(){ "i","o","t","s","z","i","j" };
+    private int _currentPiece = 0;
+
+    public Game()
+    {
+        InitializeComponent();
+
+        _width = 10;
+        _height = 20;
+
+        
+
+        InitialiseBoard(_width, _height);
+        PieceBag();
+       // NextPiece();
+        PlacePiece("t");
+
+        _gravityTimer = new DispatcherTimer
+        {
+            Interval = new TimeSpan(10000000),
+            IsEnabled = true
+        };
+        _gravityTimer.Tick += delegate{ SoftDrop(); };
+    }
+    /// <summary>
+    /// Set up board with columns and rows
+    /// </summary>
+    /// <param name="width"> Number of columns</param>
+    /// <param name="height"> Number of rows </param>
+    private void InitialiseBoard(int width, int height)
+    {
+
+        for (int i = 0; i < width; i++)
+            GameGrid.ColumnDefinitions.Add(new ColumnDefinition{Width = new GridLength(26)});
+        
+
+        for (int i = 0; i < height; i++)
+            GameGrid.RowDefinitions.Add(new RowDefinition{Height = new GridLength(26)});
+
+
+        _board = new bool[height + 2, width + 2];
+        for (int i = 0; i < width + 2; i++)
+        {
+            _board[height, i] = _board[height+1,i] = true;
+        }
+
+        for (int i = 0; i < height; i++)
+        {
+            _board[i, 0] = true;
+            _board[i, width + 1] = true;
+        }
+    }
+
+    /// <summary>
+    /// Add image to top of grid
+    /// </summary>
+    /// <param name="name"> The name of the piece to add</param>
+    private void PlacePiece(string name)
+    {
+        var piece = new Piece(name);
+
+        Grid.SetColumn(piece,5-piece.ActualBlockWidth/2);
+        Grid.SetRow(piece,0);
+        Grid.SetColumnSpan(piece,piece.ActualBlockWidth);
+        Grid.SetRowSpan(piece, piece.ActualBlockHeight);
+        piece.Stretch = Stretch.Fill;
+
+        GameGrid.Children.Add(piece);
+        _fallingPiece = piece;
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.A: ShiftLeft(); break;
+            case Key.S: SoftDrop(); break;
+            case Key.D: ShiftRight(); break;
+            case Key.Q: RotateLeft(); break;
+            case Key.E: RotateRight(); break;
+            case Key.W: HardDrop(); break;
+        }
+    }
+
+    private void ShiftRight()
+    {
+
+        _fallingPiece.Column++;
+        if (Piece.Collision(_board, _fallingPiece))
+            _fallingPiece.Column--;
+    }
+
+    private void ShiftLeft()
+    {
+        if (_fallingPiece.Column == 0) return;
+        _fallingPiece.Column--;
+        if (Piece.Collision(_board, _fallingPiece))
+            _fallingPiece.Column++;
+
+    }
+
+    private void RotateLeft()
+    {
+        _fallingPiece.RotateLeft();
+        
+    }
+
+    private void RotateRight()
+    {
+        _fallingPiece.RotateRight();
+    }
+
+    private void SoftDrop()
+    {
+        _fallingPiece.Row++;
+        if (Piece.Collision(_board, _fallingPiece))
+        {
+            _fallingPiece.Row--;
+            SetPiece();
+        }
+    }
+
+    private void SetPiece()
+    {
+        for (int row = 0; row < _fallingPiece.BlockSize; row++)
+        {
+            for (int column = 0; column < _fallingPiece.BlockSize; column++)
+            {
+                _board[row + _fallingPiece.Row, column + _fallingPiece.Column] =
+                    _board[row + _fallingPiece.Row, column + _fallingPiece.Column]
+                    || _fallingPiece.ActualLayout[row, column];
+            }
+        }
+
+        NextPiece();
+    }
+
+    private void PieceBag()
+    {
+        _currentPiece = 0;
+        var bag = _pieces.FindAll(x => true);
+        var rand = new Random();
+        var output = new List<string>();
+        while (output.Count<7)
+        {
+            var num = rand.Next(bag.Count);
+            output.Add(bag[num]);
+            bag.RemoveAt(num);
+        }
+
+        _pieces = output;
+    }
+
+    private void NextPiece()
+    {
+        if (_currentPiece==7) PieceBag();
+        var piece = _pieces[_currentPiece];
+        _currentPiece++;
+        PlacePiece(piece);
+    }
+
+    private void HardDrop()
+    {
+        var row = _fallingPiece.Row;
+        while (_fallingPiece.Row<row)
+        {
+            SoftDrop();
+        }
+    }
+}
---
+++ WpfApp1/Piece.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Accessibility;
+
+namespace Tetris;
+/// <summary>
+/// Inherits from Image, adding properties and methods for pieces
+/// </summary>
+internal class Piece : Image
+{
+    #region Properties
+    /// <summary>
+    /// Gets the default, un-rotated layout of the piece
+    /// </summary>
+    public bool[,] Layout { get; }
+    /// <summary>
+    /// Gets the name (letter) of the piece
+    /// </summary>
+    public string PieceName { get; }
+    /// <summary>
+    /// Gets/ sets a int representing rotation 90 degrees clockwise
+    /// </summary>
+    public int Rotation { get; private set; }
+    /// <summary>
+    /// Gets the height of the basic piece in blocks
+    /// </summary>
+    public int BlockSize => Layout.GetLength(0);
+    /// <summary>
+    /// Gets the width of the rotated piece in blocks
+    /// </summary>
+    public int[] Lowest
+    {
+        get
+        {
+            var output = new int[BlockSize];
+
+            for (int width = 0; width < BlockSize; width++)
+            {
+                for (int height = BlockSize - 1; height >= 0; height--)
+                {
+                    if (ActualLayout[height, width])
+                    {
+                        output[width] = BlockSize - height - 1;
+                        break;
+                    }
+                }
+            }
+            return output;
+        }
+    }
+    /// <summary>
+    /// Gets the layout of the block in it's rotation
+    /// </summary>
+    public bool[,] ActualLayout
+    {
+        get
+        {
+            var rotate = Rotation;
+            var output = Layout.Clone() as bool[,];
+            if (PieceName == "o") return Layout;
+            if (PieceName == "i")
+                switch (rotate)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        rotate += 2;
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        rotate -= 2;
+                        break;
+                    default:
+                        throw new ArgumentException("Invalid rotation");
+                }
+            bool[,] Transpose(bool[,] input)
+            {
+                var output = new bool[BlockSize,BlockSize];
+                for (int row = 0; row < BlockSize; row++)
+                {
+                    for (int column = 0; column < BlockSize; column++)
+                    {
+                        output[row,column] = input[column, row];
+                    }
+                }
+                return output; 
+            }
+
+            bool[,] Flip(bool[,] input)
+            {
+                var output = new bool[BlockSize,BlockSize];
+                for (int row = 0; row < BlockSize; row++)
+                {
+                    for (int column = 0; column < BlockSize; column++)
+                    {
+                        output[row,column] = input[row, BlockSize-column-1];
+                    }
+                }
+                return output;
+            }
+            for (int i = 0; i < rotate; i++)
+            {
+                output = Transpose(output ?? throw new InvalidOperationException());
+                output = Flip(output);
+            }
+
+            return output;
+        }
+    }
+    /// <summary>
+    /// The size of one block, in pixels
+    /// </summary>
+    private readonly int _minoSize = 26;
+    /// <summary>
+    /// The X of the centre of rotation for the piece, in pixels
+    /// </summary>
+    public double CentreX { get; }
+    /// <summary>
+    /// The Y of the centre of rotation for the piece, in pixels
+    /// </summary>
+    public double CentreY { get; }
+
+    /// <summary>
+    /// The Column position of the piece in the grid
+    /// </summary>
+    public int Column
+    {
+        get=> Grid.GetColumn(this);
+        set=> Grid.SetColumn(this, value);
+    }
+    /// <summary>
+    /// The Row position of the piece in the grid
+    /// </summary>
+    public int Row
+    {
+        get => Grid.GetRow(this);
+        set
+        {
+            Grid.SetRow(this, value);
+            Stopped = 0;
+        }
+    }
+    /// <summary>
+    /// The number of times the piece has stopped
+    /// </summary>
+    public int Stopped { get; set; }
+    /// <summary>
+    /// The number of cells high the rotated piece occupies
+    /// </summary>
+    public int ActualBlockHeight => Rotation % 2 == 0 ? height : width;
+
+    /// <summary>
+    /// The number of cells wide the piece occupies
+    /// </summary>
+    public int ActualBlockWidth => Rotation % 2 == 0 ? width : height;
+    #endregion
+    #region Private attributes
+
+    private readonly int width;
+    private readonly int height;
+    #endregion
+    #region Constructors
+    public Piece(string pieceName)
+    {
+        Rotation = 0;
+        PieceName = pieceName;
+        switch (pieceName.ToLower())
+        {
+            case "j":
+                Layout = new[,]
+                {
+                    { true, false, false },
+                    { true, true, true },
+                    { false, false, false }
+                };
+                break;
+            case "l":
+                Layout = new[,]
+                {
+                    { false, false, true },
+                    { true, true, true },
+                    { false, false, false }
+                };
+                break;
+            case "t":
+                Layout = new[,]
+                {
+                    { false, true, false },
+                    { true, true, true },
+                    { false, false, false }
+                };
+                break;
+            case "i":
+                Layout = new[,]
+                {
+                    { false, false, false, false },
+                    { true, true, true, true },
+                    { false, false, false, false },
+                    { false, false, false, false }
+                };
+                break;
+            case "o":
+                Layout = new[,]
+                {
+                    { false, true, true, false },
+                    { false, true, true, false },
+                    { false, false, false, false },
+                    { false, false, false, false }
+                };
+                break;
+            case "s":
+                Layout = new[,]
+                {
+                    { false, true, true },
+                    { true, true, false },
+                    { false, false, false }
+                };
+                break;
+            case "z":
+                Layout = new[,]
+                {
+                    { true, true, false },
+                    { false, true, true },
+                    { false, false, false }
+                };
+                break;
+            default:
+                throw new ArgumentException("Invalid Piece");
+        }
+
+        if (pieceName is "s" or "z" or "j" or "l" or "t")
+        {
+            CentreX = CentreY = _minoSize * 1.5;
+            width = 3;
+            height = 2;
+        }
+        else if (pieceName == "o")
+        {
+            CentreX = _minoSize * 2;
+            CentreY = _minoSize;
+            width = height = 2;
+        }
+        else
+        {
+            CentreY = CentreX = 2 * _minoSize;
+            width = 4;
+            height = 1;
+        }
+
+        Source = new BitmapImage(new Uri(@"C:\Users\jhp33\source\repos\School\Tetris\WpfApp1\WpfApp1\Resources\Tetrominoes\" + PieceName + ".png"));
+    }
+    #endregion
+    
+    #region Public methods
+    public void RotateRight()
+    {
+        Rotation++;
+        if (Rotation == 4) Rotation = 0;
+
+        var transform = new TransformGroup();
+        transform.Children.Add(new RotateTransform(90,CentreX,CentreY));
+        transform.Children.Add(RenderTransform);
+        RenderTransform = transform;
+        Grid.SetRowSpan(this,ActualBlockWidth);
+        Grid.SetColumnSpan(this,ActualBlockHeight);
+    }
+
+    public void RotateLeft()
+    {
+        Rotation--;
+        if (Rotation == -1) Rotation = 3;
+
+        var transform = new TransformGroup();
+        transform.Children.Add(new RotateTransform(-90,CentreX,CentreY));
+        transform.Children.Add(RenderTransform);
+        RenderTransform = transform;
+        Grid.SetRowSpan(this,ActualBlockWidth);
+        Grid.SetColumnSpan(this,ActualBlockHeight);
+    }
+
+    #endregion
+
+    #region Public static methods
+    public static bool Collision(bool[,] board, Piece piece)
+    {
+        var x = piece.Column;
+        var y = piece.Row;
+        for (int row = 0; row < piece.BlockSize; row++)
+        {
+            for (int column = 0; column < piece.BlockSize; column++)
+            {
+                if (piece.ActualLayout[row, column] && board[row + y, column + x+1])
+                    return true;
+            }
+        }
+        return false;
+    }
+    #endregion
+}
